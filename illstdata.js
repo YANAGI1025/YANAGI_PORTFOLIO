2287c55f671207d144e436652439035cff337604
YANAGI1025 <190383463+YANAGI1025@users.noreply.github.com>
2026-09-10 02:21:15 +0900

    更新

---
+++ illstdata.js
@@ -0,0 +1,87 @@
+function getAllTags() {
+  return ['すべて', 'OC', 'よその子', '版権'];
+}
+
+const ARTWORKS = [
+  {
+    id: 0,
+    title: 'プリッツ資料',
+    img: 'img/illust/PRITTS_2025_09.png',
+    tags: ['OC'], date: '2025-09-04', desc: 'うちの子の資料'
+  },
+  {
+    id: 11,
+    title: 'プリッツ',
+    img: 'img/illust/260714.png',
+    tags: ['OC'], date: '2026-07-14', desc: 'うちの子プリッツ'
+  },
+  {
+    id: 10,
+    title: '楽さん',
+    img: 'img/illust/260527.png',
+    tags: ['よその子'], date: '2026-05-27', desc: 'アカシリクさん宅の子です'
+  },
+  {
+    id: 9,
+    title: 'Brain rot',
+    img: 'img/illust/260314.png',
+    tags: ['版権'], date: '2026-03-14', desc: 'FA『東京真中 - ブレインロット feat. 重音テト』'
+  },
+  {
+    id: 8,
+    title: 'あけおめ 2026',
+    img: 'img/illust/260101.png',
+    tags: ['OC'], date: '2026-01-01', desc: 'あけおめ！'
+  },
+  {
+    id: 7,
+    title: 'みけつぁん',
+    img: 'img/illust/250816.png',
+    tags: ['よその子'], date: '2025-08-16', desc: 'しあわせさん宅のみけつぁんです'
+  },
+  {
+    id: 6,
+    title: 'レマさん',
+    img: 'img/illust/2508XX.png',
+    tags: ['よその子'], date: '2025-08-XX', desc: 'レンマさん宅のレマさんです'
+  },
+  {
+    id: 5,
+    title: 'プリッツ',
+    img: 'img/illust/250709.png',
+    tags: ['OC'], date: '2025-07-09', desc: '振り向くうちの子'
+  },
+  {
+    id: 4,
+    title: 'ディルくん',
+    img: 'img/illust/250610.png',
+    tags: ['よその子'], date: '2025-06-10', desc: 'ちみんさん宅の子です'
+  },
+  {
+    id: 3,
+    title: '稲熊くん',
+    img: 'img/illust/250427.png',
+    tags: ['よその子'], date: '2025-04-27', desc: 'ふるかわかこさん宅の子です'
+  },
+  {
+    id: 2,
+    title: 'あけおめ 2025',
+    img: 'img/illust/241231.png',
+    tags: ['OC'], date: '2025-01-01', desc: 'あけおめ！'
+  },
+  {
+    id: 1,
+    title: 'ランさん',
+    img: 'img/illust/240601.png',
+    tags: ['よその子'], date: '2024-06-01', desc: 'ミレさん宅の子です'
+  },
+];
+
+/*
+  {
+    id: ,
+    title: '',
+    img: 'img/illust/',
+    tags: [''], date: '', desc: ''
+  },
+  */
