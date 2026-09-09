73fa78544d06bd7dd79ebc615272f4be1ef1b6f1
YANAGI1025 <190383463+YANAGI1025@users.noreply.github.com>
2026-09-09 16:00:37 +0900

    更新

--- js/illstdata.js
+++ js/illstdata.js
@@ -1,35 +1,86 @@
-    function getAllTags(){ return ['よその子','OC','版権','落書き']; }
+function getAllTags() {
+  return ['すべて', 'OC', 'よその子', '版権'];
+}
 
-    const ARTWORKS = [
-      {id:0,title:'プリッツ資料',img:'https://pbs.twimg.com/media/Gz_YH_ha0AAxJHL?format=jpg&name=4096x4096',tags:['OC'],date:'2025-09-04',
-      desc:'こんな感じの子です'},
-      {id:1,title:'プリッツ',img:'https://pbs.twimg.com/media/GvWLrzyWcAAKJpV?format=jpg&name=4096x4096',tags:['OC'],date:'2025-07-09',
-      desc:'長髪をバッサリ切った記念のうちの子絵'},
+const ARTWORKS = [
+  {
+    id: 0,
+    title: 'プリッツ資料',
+    img: 'https://pbs.twimg.com/media/Gz_YH_ha0AAxJHL?format=jpg&name=4096x4096',
+    tags: ['OC'],
+    date: '2025-09-04',
+    desc: 'こんな感じの子です'
+  },
 
-      {id:7,title:'シグ',img:'https://pbs.twimg.com/media/G1QlaGYbQAAcQ8f?format=jpg&name=4096x4096',tags:['版権'],date:'2025-09-20',
-      desc:'Steam版ぷよテト2、レート2000↑達成記念シグシグ'},
+  {
+    id: 1,
+    title: 'プリッツ',
+    img: 'https://pbs.twimg.com/media/GvWLrzyWcAAKJpV?format=jpg&name=4096x4096',
+    tags: ['OC'],
+    date: '2025-07-09',
+    desc: '長髪をバッサリ切った記念のうちの子絵'
+  },
 
-      {id:6,title:'せいれんさん',img:'https://pbs.twimg.com/media/G08gAmXaEAAD97X?format=jpg&name=4096x4096',tags:['よその子'],date:'2025-09-16',
-      desc:''},
+  {
+    id: 7,
+    title: 'シグ',
+    img: 'https://pbs.twimg.com/media/G1QlaGYbQAAcQ8f?format=jpg&name=4096x4096',
+    tags: ['版権'],
+    date: '2025-09-20',
+    desc: 'Steam版ぷよテト2、レート2000↑達成記念シグシグ'
+  },
 
-      {id:5,title:'ルイさん',img:'https://pbs.twimg.com/media/GzRd761awAARdYF?format=jpg&name=4096x4096',tags:['よその子'],date:'2025-08-26',
-      desc:''},
+  {
+    id: 6,
+    title: 'せいれんさん',
+    img: 'https://pbs.twimg.com/media/G08gAmXaEAAD97X?format=jpg&name=4096x4096',
+    tags: ['よその子'],
+    date: '2025-09-16',
+    desc: ''
+  },
 
-      {id:4,title:'レマさん',img:'https://pbs.twimg.com/media/Gy4wITfbcAAoyS5?format=jpg&name=4096x4096',tags:['よその子'],date:'2025-08-20',
-      desc:''},
+  {
+    id: 5,
+    title: 'ルイさん',
+    img: 'https://pbs.twimg.com/media/GzRd761awAARdYF?format=jpg&name=4096x4096',
+    tags: ['よその子'],
+    date: '2025-08-26',
+    desc: ''
+  },
 
-      {id:3,title:'ミケつぁん',img:'https://pbs.twimg.com/media/Gya3s9ebQAA3X7x?format=jpg&name=4096x4096',tags:['よその子'],date:'2025-08-16',
-      desc:''},
+  {
+    id: 4,
+    title: 'レマさん',
+    img: 'https://pbs.twimg.com/media/Gy4wITfbcAAoyS5?format=jpg&name=4096x4096',
+    tags: ['よその子'],
+    date: '2025-08-20',
+    desc: ''
+  },
 
-      {id:2,title:'カタリスさん',img:'https://pbs.twimg.com/media/GyXcSkGa4AQ6f3g?format=jpg&name=4096x4096',tags:['よその子'],date:'2025-08-15',
-      desc:''},
+  {
+    id: 3,
+    title: 'ミケつぁん',
+    img: 'https://pbs.twimg.com/media/Gya3s9ebQAA3X7x?format=jpg&name=4096x4096',
+    tags: ['よその子'],
+    date: '2025-08-16',
+    desc: ''
+  },
 
-      {id:0,title:'新プリッツ',img:'https://pbs.twimg.com/media/Gt0gD8dWcAE5Nyq?format=jpg&name=4096x4096',tags:['OC','落書き'],date:'2025-06-20',
-      desc:'髪を短くしました'},
+  {
+    id: 2,
+    title: 'カタリスさん',
+    img: 'https://pbs.twimg.com/media/GyXcSkGa4AQ6f3g?format=jpg&name=4096x4096',
+    tags: ['よその子'],
+    date: '2025-08-15',
+    desc: ''
+  },
 
-    ];
-
-    /*
-      {id:,title:'',img:'',tags:['よその子'],date:'',
-      desc:''},
-      */
+  {
+    id: 0,
+    title: '新プリッツ',
+    img: 'https://pbs.twimg.com/media/Gt0gD8dWcAE5Nyq?format=jpg&name=4096x4096',
+    tags: ['OC'],
+    date: '2025-06-20',
+    desc: '髪を短くしました'
+  }
+];
