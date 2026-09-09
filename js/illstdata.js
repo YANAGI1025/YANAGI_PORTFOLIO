d441b7c237f2210b1dde1175e6892138f02d56df
YANAGI1025 <190383463+YANAGI1025@users.noreply.github.com>
2026-09-10 00:09:12 +0900

    更新

--- js/illstdata.js
+++ js/illstdata.js
@@ -6,81 +6,82 @@ const ARTWORKS = [
   {
     id: 0,
     title: 'プリッツ資料',
-    img: 'https://pbs.twimg.com/media/Gz_YH_ha0AAxJHL?format=jpg&name=4096x4096',
-    tags: ['OC'],
-    date: '2025-09-04',
-    desc: 'こんな感じの子です'
+    img: 'img/illust/PRITTS_2025_09.png',
+    tags: ['OC'], date: '2025-09-04', desc: 'うちの子の資料'
   },
-
   {
-    id: 1,
+    id: 11,
     title: 'プリッツ',
-    img: 'https://pbs.twimg.com/media/GvWLrzyWcAAKJpV?format=jpg&name=4096x4096',
-    tags: ['OC'],
-    date: '2025-07-09',
-    desc: '長髪をバッサリ切った記念のうちの子絵'
+    img: 'img/illust/260714.png',
+    tags: ['OC'], date: '2026-07-14', desc: 'うちの子プリッツ'
   },
-
+  {
+    id: 10,
+    title: '楽さん',
+    img: 'img/illust/260527.png',
+    tags: ['よその子'], date: '2026-05-27', desc: 'アカシリクさん宅の子です'
+  },
+  {
+    id: 9,
+    title: 'Brain rot',
+    img: 'img/illust/260314.png',
+    tags: ['版権'], date: '2026-03-14', desc: 'FA『東京真中 - ブレインロット feat. 重音テト』'
+  },
+  {
+    id: 8,
+    title: 'あけおめ 2026',
+    img: 'img/illust/260101.png',
+    tags: ['OC'], date: '2026-01-01', desc: 'あけおめ！'
+  },
   {
     id: 7,
-    title: 'シグ',
-    img: 'https://pbs.twimg.com/media/G1QlaGYbQAAcQ8f?format=jpg&name=4096x4096',
-    tags: ['版権'],
-    date: '2025-09-20',
-    desc: 'Steam版ぷよテト2、レート2000↑達成記念シグシグ'
+    title: 'みけつぁん',
+    img: 'img/illust/250816.png',
+    tags: ['よその子'], date: '2025-08-16', desc: 'しあわせさん宅のみけつぁんです'
   },
-
   {
     id: 6,
-    title: 'せいれんさん',
-    img: 'https://pbs.twimg.com/media/G08gAmXaEAAD97X?format=jpg&name=4096x4096',
-    tags: ['よその子'],
-    date: '2025-09-16',
-    desc: ''
+    title: 'レマさん',
+    img: 'img/illust/2508XX.png',
+    tags: ['よその子'], date: '2025-08-XX', desc: 'レンマさん宅のレマさんです'
   },
-
   {
     id: 5,
-    title: 'ルイさん',
-    img: 'https://pbs.twimg.com/media/GzRd761awAARdYF?format=jpg&name=4096x4096',
-    tags: ['よその子'],
-    date: '2025-08-26',
-    desc: ''
+    title: 'プリッツ',
+    img: 'img/illust/250709.png',
+    tags: ['OC'], date: '2025-07-09', desc: '振り向くうちの子'
   },
-
   {
     id: 4,
-    title: 'レマさん',
-    img: 'https://pbs.twimg.com/media/Gy4wITfbcAAoyS5?format=jpg&name=4096x4096',
-    tags: ['よその子'],
-    date: '2025-08-20',
-    desc: ''
+    title: 'ディルくん',
+    img: 'img/illust/250610.png',
+    tags: ['よその子'], date: '2025-06-10', desc: 'ちみんさん宅の子です'
   },
-
   {
     id: 3,
-    title: 'ミケつぁん',
-    img: 'https://pbs.twimg.com/media/Gya3s9ebQAA3X7x?format=jpg&name=4096x4096',
-    tags: ['よその子'],
-    date: '2025-08-16',
-    desc: ''
+    title: '稲熊くん',
+    img: 'img/illust/250427.png',
+    tags: ['よその子'], date: '2025-04-27', desc: 'ふるかわかこさん宅の子です'
   },
-
   {
     id: 2,
-    title: 'カタリスさん',
-    img: 'https://pbs.twimg.com/media/GyXcSkGa4AQ6f3g?format=jpg&name=4096x4096',
-    tags: ['よその子'],
-    date: '2025-08-15',
-    desc: ''
+    title: 'あけおめ 2025',
+    img: 'img/illust/241231.png',
+    tags: ['OC'], date: '2025-01-01', desc: 'あけおめ！'
   },
-
   {
-    id: 0,
-    title: '新プリッツ',
-    img: 'https://pbs.twimg.com/media/Gt0gD8dWcAE5Nyq?format=jpg&name=4096x4096',
-    tags: ['OC'],
-    date: '2025-06-20',
-    desc: '髪を短くしました'
-  }
+    id: 1,
+    title: 'ランさん',
+    img: 'img/illust/240601.png',
+    tags: ['よその子'], date: '2024-06-01', desc: 'ミレさん宅の子です'
+  },
 ];
+
+/*
+  {
+    id: ,
+    title: '',
+    img: 'img/illust/',
+    tags: [''], date: '', desc: ''
+  },
+  */
